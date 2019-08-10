0cd12840eb20b283d3f90455227e726a02ef139e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-08-10 21:45:06 +0000

    Fix bug in crediting overdrawn balances and add checks for credit and debit amounts being appropriately signed

--- R/Balance.R
+++ R/Balance.R
@@ -23,10 +23,20 @@ Balance <- R6::R6Class(
       self$balance <- sum(sapply(self$chunks, function(c) c$value))
     },
     credit = function(amount) {
-      self$chunks <- c(Chunk$new(amount), self$chunks)
+      stopifnot(amount > 0)
+      amount_to_credit <- amount
+      if (is(self$chunks[[1]], "BaseChunk")) {
+        amount_to_credit_to_chunk <- min(abs(self$chunks[[1]]$value), amount_to_credit)
+        self$chunks[[1]]$credit(amount_to_credit_to_chunk)
+        amount_to_credit <- amount_to_credit - amount_to_credit_to_chunk
+      }
+      if (amount_to_credit > 0) {
+        self$chunks <- c(Chunk$new(amount_to_credit), self$chunks)
+      }
       self$calculate_balance()
     },
     debit = function(amount) {
+      stopifnot(amount < 0)
       amount_to_debit <- amount
       while (amount_to_debit > 0) {
         if (is(self$chunks[[1]], "BaseChunk")) {
@@ -40,6 +50,7 @@ Balance <- R6::R6Class(
           }
         }
         amount_to_debit <- amount_to_debit - amount_to_take_from_chunk
+        self$calculate_balance()
       }
     }
   )
